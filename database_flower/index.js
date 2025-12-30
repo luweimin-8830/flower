b6b4714d9cad27c0198016fb85eb5c6b900f70d6
weiminlu <1228225481@qq.com>
2025-12-30 14:53:43 +0800

    init

---
+++ database_flower/index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { init: initDB, Counter } = require("./db");
+const apiRouter = require("./src/api/index.js");
+import { errorHandler,notFoundHandler } from "./src/error.js";
+import { ok,fail } from "./src/response.js";
+
+
+const logger = morgan("tiny");
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(logger);
+app.use("/api",apiRouter);
+
+// 首页
+app.get("/", async (req, res) => {
+  res.send("Hello");
+});
+
+// 更新计数
+app.post("/api/count", async (req, res) => {
+  const { action } = req.body;
+  if (action === "inc") {
+    await Counter.create();
+  } else if (action === "clear") {
+    await Counter.destroy({
+      truncate: true,
+    });
+  }
+  res.send({
+    code: 0,
+    data: await Counter.count(),
+  });
+});
+
+// 获取计数
+app.get("/api/count", async (req, res) => {
+  const result = await Counter.count();
+  res.send({
+    code: 0,
+    data: result,
+  });
+});
+
+// 小程序调用，获取微信 Open ID
+app.get("/api/wx_openid", async (req, res) => {
+  if (req.headers["x-wx-source"]) {
+    res.send(req.headers["x-wx-openid"]);
+  }
+});
+
+app.use(notFoundHandler);//404
+app.use(errorHandler);//500
+
+const port = process.env.PORT || 80;
+
+async function bootstrap() {
+  await initDB();
+  app.listen(port, () => {
+    console.log("启动成功", port);
+  });
+}
+
+bootstrap();
